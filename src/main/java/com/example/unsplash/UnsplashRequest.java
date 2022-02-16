c3aac67b252e6e5b115232e544f9bad8359ac83b
Meloy9992 <81082136+Meloy9992@users.noreply.github.com>
2022-02-16 09:55:52 +0300

    Update UnsplashRequest.java

--- src/main/java/com/example/unsplash/UnsplashRequest.java
+++ src/main/java/com/example/unsplash/UnsplashRequest.java
@@ -3,7 +3,7 @@ package com.example.unsplash;
 public class UnsplashRequest {
    final String API = "https://api.unsplash.com";
    final String SEARCH = "/search/photos";
-   final String CLIENT_ID = "client_id=v3o0jU3i0pFwqYkpj6tZRRUK7_Y3D_FyflsOBbpJ77g";
+   final String CLIENT_ID = "client_id=";//Your API key
 
     public String search (String userQuery){
         String searchPhoto = API +  SEARCH + "?query=" + userQuery + "&" + CLIENT_ID;
